442ca6591f186903a800559013f5815667fb4ad2
Alejo Caeiro <acaeiro@itba.edu.ar>
2022-05-27 15:51:24 -0300

    Cambios en shell para recibir argumentos

--- Userland/SampleCodeModule/shell.c
+++ Userland/SampleCodeModule/shell.c
@@ -5,41 +5,68 @@
 
 #define STDIN 0
 #define USER 2
-#define SIZE_BUF 200
+#define SIZE_BUF 100
+#define MAX_ARGUMENTS 7
 
 static char exit;
-static char buffer[SIZE_BUF]={0};
+
 static char user[SIZE_BUF]={0};
 static int userLen;
 
-static void getBuffer();
+static int getBuffer(char*, char** );
 
 /*Funcion que se encarga de leer lo que ingrese el usuario constantemente hasta que escriba exit*/
 void shell(){
     printf("Ingrese nombre de usuario: ");
-    getBuffer();
-    strCpy(user,buffer);
-    userLen=strLen(user);
+    userLen= sys_read(STDIN, user, SIZE_BUF);
+    user[--userLen]=0;
 
     printf("\nBienvenido!\nComando \'/help\' para mas ayuda\n\n");
     printf("Presione 'CTRL' en cualquier momento para guardar el valor de los registros\n");
     printf("Que modulo desea correr?\n");
-    getBuffer();
-    exit = runCommand(checkCommand(buffer));
+
+    int argc;
+    char *argv[MAX_ARGUMENTS] = {0};
+    int buff_size;
+    char buffer[SIZE_BUF]={0};
+
+    //getBuffer();
+    //exit = runCommand(checkCommand(buffer));
 
     while (!exit){
         putChar('$');
         sys_write(USER,user,userLen);
         printf(": ");
 
-        getBuffer();
+        buff_size=sys_read(STDIN, buffer, SIZE_BUF);
+        buffer[buff_size-1]=0;
 
-        exit = runCommand(checkCommand(buffer));
+        argc = getBuffer(buffer,argv);
+
+        exit = runCommand(checkCommand(argv[0]));
     }
 }
 
 /*Lee de entrada estandar lo que escribe el usuario*/
-static void getBuffer(){
-    int len = sys_read(STDIN, buffer, SIZE_BUF);
-    buffer[len-1] = 0;
+static int getBuffer(char* buffer, char** argv){
+
+    int argc = 0;
+
+    if (*buffer != ' ' && *buffer != '\0') {
+        argv[argc++] = buffer;
+    }
+
+    while (*buffer != 0) {
+        if (*buffer == ' ') {
+        *buffer = 0;
+            if ((*(buffer + 1) != ' ') && (*(buffer + 1) != 0)) {
+                if (argc >= MAX_ARGUMENTS) {
+                    return -1;
+                }
+                argv[argc++] = buffer + 1;
+            }
+        }
+        buffer++;
+    }
+    return argc;
 }
